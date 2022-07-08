89ec295b963cebeeeac365646687858506394218
Hung1029 <psi94133@gmail.com>
2022-07-08 22:57:20 +0800

    ButterFly_SHOW

--- Assets/ButterflyOnPalm/Scripts/PlaceOnPalm.cs
+++ Assets/ButterflyOnPalm/Scripts/PlaceOnPalm.cs
@@ -1,52 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 public class PlaceOnPalm : MonoBehaviour
 {
-    [SerializeField] private FingerInfoGizmo fingerInfoGizmo;
-
     bool showButterfly = true;
+    public Text ButterFlyPos;
+    public Text ButterFlyShow;
+    public Text HandState;
 
     private void Start()
     {
-        Screen.orientation = ScreenOrientation.Portrait;
-        if (fingerInfoGizmo == null) 
-        {
-            try
-            {
-                fingerInfoGizmo = GameObject.Find("TryOnManager").GetComponent<FingerInfoGizmo>();
-                
-            }
-            catch
-            {
-                Debug.Log("Can't find 'TryOnManager' GameObject");
-            }
+        ButterFlyPos.text = "INIT";
+        ButterFlyShow.text = "INIT";
+        HandState.text = "INIT";
 
-        
-        }
         foreach (var mr in GetComponentsInChildren<MeshRenderer>())
         {
-            mr.enabled = false;
+            mr.enabled = true;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
-
-        if (ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info.mano_class == ManoClass.GRAB_GESTURE)
+        if (ARSession.state == ARSessionState.SessionTracking)
         {
-           
-            HandInfo currentlyDetectedHand = ManomotionManager.Instance.Hand_infos[0].hand_info;
-            ManoGestureContinuous currentlyDetectedContinousGesture = currentlyDetectedHand.gesture_info.mano_gesture_continuous;
+            FollowPalmCenter();
+        }
+    }
 
-            Vector3 pinchPos = Vector3.Lerp(fingerInfoGizmo.LeftFingerPoint3DPosition, fingerInfoGizmo.RightFingerPoint3DPosition, 0.5f);
+    private void FollowPalmCenter()
+    {
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        HandInfo currentlyDetectedHand = ManomotionManager.Instance.Hand_infos[0].hand_info;
+        ManoGestureContinuous currentlyDetectedContinousGesture = currentlyDetectedHand.gesture_info.mano_gesture_continuous;
 
+        Vector3 pinchPos = currentlyDetectedHand.tracking_info.palm_center;
+        HandState.text = "HAND"+ pinchPos.ToString();
+
+        if (currentlyDetectedContinousGesture == ManoGestureContinuous.OPEN_HAND_GESTURE)
+        {
+            HandState.text = "OPENHAND " + pinchPos.ToString();
             Vector3 newPos = ManoUtils.Instance.CalculateNewPosition(pinchPos, currentlyDetectedHand.tracking_info.depth_estimation);
-
             if (!showButterfly)
             {
                 foreach (var mr in meshRenderers)
@@ -55,16 +53,19 @@ public class PlaceOnPalm : MonoBehaviour
                 }
                 transform.position = newPos;
                 showButterfly = true;
+                ButterFlyPos.text = transform.position.ToString();
+                ButterFlyShow.text = "TRUE";
             }
             else
             {
                 transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 5f);
+                ButterFlyPos.text = transform.position.ToString();
+                ButterFlyShow.text = "TRUE";
             }
-
-
-
         }
-        else {
+        else
+        {
+            HandState.text = "NO HAND";
             if (showButterfly)
             {
                 foreach (var mr in meshRenderers)
@@ -72,9 +73,10 @@ public class PlaceOnPalm : MonoBehaviour
                     mr.enabled = false;
                 }
                 showButterfly = false;
+                ButterFlyPos.text = "Missing";
+                ButterFlyShow.text = "FALSE";
+
             }
-
         }
-
-    }
+        }
 }
